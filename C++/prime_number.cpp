20b548760625323db03cc6e9d1e6fc76ebe15f8d
pafup <91951203+pafup@users.noreply.github.com>
2022-10-04 17:21:43 +0530

    Updated to reduce the time complexity
    reduced the time complexity from O(N^2) to O(N^(3/2)

--- C++/prime_number.cpp
+++ C++/prime_number.cpp
@@ -1,24 +1,42 @@
+/ Prime numbers till N
 #include <bits/stdc++.h>
 using namespace std;
   
+// Function to check if a given 
+// number is prime
 bool isPrime(int n)
 {
-    // Corner case
-    if (n <= 1)
-        return false;
+  // Since 0 and 1 is not prime 
+  // return false.
+  if(n == 1 || n == 0)return false;
+    
+  // Run a loop from 2 to 
+  // square root of n.
+  for(int i = 2; i * i <= n; i++)
+  {
+    // If the number is divisible by i, 
+    // then n is not a prime number.
+    if(n % i == 0)return false;
+  }
   
- 
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-  
-    return true;
+  // Otherwise n is a prime number.
+  return true;
 }
   
-
+// Driver code
 int main()
 {
-    isPrime(11) ? cout << " true\n" : cout << " false\n";
-    isPrime(15) ? cout << " true\n" : cout << " false\n";
+    int N = 100;
+  
+      // Check for every number from 1 to N
+      for(int i = 1; i <= N; i++)
+      {
+      // Check if current number is prime
+      if(isPrime(i)) 
+      {
+        cout << i << " ";
+      }
+    }
+  
     return 0;
 }
